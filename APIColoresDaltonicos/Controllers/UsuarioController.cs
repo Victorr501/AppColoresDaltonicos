80b32745dd811f0e37e193eb72cbf6aba34735be
Víctor Rubín Rubio <VrubinR501@outlook.com>
2026-03-07 18:41:26 +0100

    Terminar linea de usuarios

--- APIColoresDaltonicos/Controllers/UsuarioController.cs
+++ APIColoresDaltonicos/Controllers/UsuarioController.cs
@@ -1,13 +1,16 @@
-﻿using APIColoresDaltonicos.Services.Services.Usuarios;
+﻿using APIColoresDaltonicos.Models.Usuarios.DTOs;
+using APIColoresDaltonicos.Services.Services.Usuarios;
+using APIColoresDaltonicos.Models.Usuarios;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using APIColoresDaltonicos.Services.Excepcion;
 
 namespace APIColoresDaltonicos.Controllers
 {
 
     [Route("api/[controller]")]
     [ApiController]
-    public class UsuarioController : Controller
+    public class UsuarioController : ControllerBase
     {
 
         private readonly IUsuarioService _usuarioService;
@@ -21,7 +24,87 @@ namespace APIColoresDaltonicos.Controllers
             _logger = logger;
         }
 
+        [HttpPost("registrar")]
+        public async Task<IActionResult> Registrar([FromBody] RegistroUsuarioDto peticion)
+        {
+            try
+            {
+                var nuevoUsuario = _mapper.Map<Usuario>(peticion);
 
+                var resultadoDto = await _usuarioService.RegistrarUsuarioAsync(nuevoUsuario);
+
+                return Ok(resultadoDto);
+            }
+            catch (EmailDuplicadoException ex)
+            {
+                return BadRequest(new { mensaje = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto login)
+        {
+            try
+            {
+                var resultadoDto = await _usuarioService.LoginAsync(login.Email, login.Password);
+
+                return Ok(resultadoDto);
+            }
+            catch (CredencialesInvalidasException ex)
+            {
+                return Unauthorized(new { mensaje = ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObtenerUsuarioSeguro(int id) {
+            try
+            {
+                var usuarioDto = await _usuarioService.ObtenerUsuarioSeguroPorIdAsync(id);
+                return Ok(usuarioDto);
+            }
+            catch (UsuarioNoEncontradoException ex)
+            {
+                return NotFound(new { mensaje = ex.Message });
+            }
+        }
+
+        [HttpPut("perfil")]
+        public async Task<IActionResult> ActualizarPerfil([FromBody] UsuarioResponseDto usuarioActualizar)
+        {
+            try
+            {
+                var resultadoDto = await _usuarioService.ActualizarPerfilAsync(usuarioActualizar);
+                return Ok(resultadoDto);
+            }
+            catch (UsuarioNoEncontradoException ex)
+            {
+                return NotFound(new { mensaje = ex.Message });
+            }
+            catch (EmailDuplicadoException ex)
+            {
+                return BadRequest(new { mensaje = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}/password")]
+        public async Task<IActionResult> CambiarPassword(int id, [FromBody] CambiarPasswordDto passwords)
+        {
+            try
+            {
+                await _usuarioService.ActualizarPasswordAsync(id, passwords.PasswordActual, passwords.PasswordNueva);
+
+                return NoContent();
+            }
+            catch (CredencialesInvalidasException ex)
+            {
+                return BadRequest(new { mensaje = ex.Message });
+            }
+            catch (UsuarioNoEncontradoException ex) 
+            { 
+                return NotFound(new { mensaje = ex.Message });
+            }
+        }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> BorrarUsuario(int id)
